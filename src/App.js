af2f1720dc553c8d5af944d96e5cad492275cf26
Shuber <shuberinu@gmail.com>
2022-07-05 11:37:34 +0700

    add app markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+	const [budget, setBudget] = useState(0);
 	return (
 		<div className='card'>
 			<h1 className='title'>SNH48 Vote Budgeter</h1>
+			<form>
+				<label>Enter your budget:</label>
+				<input type='number'></input>
+				<select>
+					<option>USD</option>
+				</select>
+			</form>
+			<ul>
+				<li>
+					<span>Amount in CNY:</span>
+					<span>100</span>
+				</li>
+				<li>
+					<span>Votes:</span>
+					<span>10</span>
+				</li>
+			</ul>
 		</div>
 	);
 }
