d469c23c2031895fe1fc3a7e3915eeecc14f390a
Shuber <shuberinu@gmail.com>
2022-07-08 11:52:31 +0700

    listen for changes in form values

--- src/App.js
+++ src/App.js
@@ -4,17 +4,40 @@ import './App.css';
 function App() {
 	const [options, setOptions] = useState({
 		budget: 0,
-		selectedCurrency: 'USD'
+		selectedCurrency: ''
 	});
+
+	function handleChange(e) {
+		const { name, value } = e.target;
+		setOptions((prevOptions) => {
+			return {
+				...prevOptions,
+				[name]: value
+			};
+		});
+	}
 
 	return (
 		<div className='card round-corner'>
 			<h1 className='title'>SNH48 Vote Budgeter</h1>
 			<form className='form'>
 				<label>Enter your budget:</label>
-				<input type='number' className='budget round-corner'></input>
-				<select className='currency round-corner'>
-					<option>USD</option>
+				<input
+					type='number'
+					className='budget round-corner'
+					placeholder='Budget...'
+					name='budget'
+					value={options.budget}
+					onChange={handleChange}
+				/>
+				<select
+					className='currency round-corner'
+					id='selectedCurrency'
+					name='selectedCurrency'
+					value={options.selectedCurrency}
+					onChange={handleChange}>
+					<option value=''>---Choose---</option>
+					<option value='USD'>USD</option>
 				</select>
 				<button type='submit' className='submit round-corner'>
 					Submit
